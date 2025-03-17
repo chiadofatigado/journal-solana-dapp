290517bab89d320f918e67762dbbb6f725123151
Sandro <sandromilhano@gmail.com>
2025-03-17 13:08:16 +0000

    Initial contract code

--- anchor/programs/journalsolanadapp/src/lib.rs
+++ anchor/programs/journalsolanadapp/src/lib.rs
@@ -1,70 +1,110 @@
 #![allow(clippy::result_large_err)]
+#![allow(unexpected_cfgs)]
 
 use anchor_lang::prelude::*;
 
-declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
+declare_id!("VqwzQTMfMMn9Q1zFpzCKMhNxR3qgAXsRPeCi5vVAimP");
+
+const ANCHOR_SIZE_DESCRIMINATOR: usize = 8;
 
 #[program]
 pub mod journalsolanadapp {
     use super::*;
 
-  pub fn close(_ctx: Context<CloseJournalsolanadapp>) -> Result<()> {
-    Ok(())
-  }
+    pub fn create_journal_entry(
+        ctx: Context<CreateJournalEntry>,
+        title: String,
+        content: String,
+    ) -> Result<()> {
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.owner = *ctx.accounts.owner.key;
+        journal_entry.title = title;
+        journal_entry.content = content;
+        msg!("Journal entry created with title: {}", journal_entry.title);
+        msg!("Journal entry content: {}", journal_entry.content);
+        msg!("Journal entry owner: {}", journal_entry.owner);
+        msg!("Journal entry address: {}", journal_entry.key());
+        Ok(())
+    }
 
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.journalsolanadapp.count = ctx.accounts.journalsolanadapp.count.checked_sub(1).unwrap();
-    Ok(())
-  }
+    pub fn update_journal_entry(
+        ctx: Context<UpdateJournalEntry>,
+        _title: String,
+        content: String,
+    ) -> Result<()> {
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.content = content;
+        Ok(())
+    }
 
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.journalsolanadapp.count = ctx.accounts.journalsolanadapp.count.checked_add(1).unwrap();
-    Ok(())
-  }
+    pub fn delete_journal_entry(
+      _ctx: Context<DeleteJournalEntry>,
+    ) -> Result<()>{
+      Ok(())
+    }
 
-  pub fn initialize(_ctx: Context<InitializeJournalsolanadapp>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.journalsolanadapp.count = value.clone();
-    Ok(())
-  }
 }
 
 #[derive(Accounts)]
-pub struct InitializeJournalsolanadapp<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
+#[instruction(title: String)]
+pub struct CreateJournalEntry<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
 
-  #[account(
-  init,
-  space = 8 + Journalsolanadapp::INIT_SPACE,
-  payer = payer
+    #[account(
+    init,
+    seeds = [title.as_bytes(), owner.key().as_ref()],
+    bump,
+    payer = owner,
+    space = ANCHOR_SIZE_DESCRIMINATOR + JournalEntryState::INIT_SPACE,
   )]
-  pub journalsolanadapp: Account<'info, Journalsolanadapp>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseJournalsolanadapp<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
+    pub journal_entry: Account<'info, JournalEntryState>,
 
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub journalsolanadapp: Account<'info, Journalsolanadapp>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct Update<'info> {
+#[instruction(title: String)]
+pub struct UpdateJournalEntry<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    #[account(
+      mut,
+      seeds = [title.as_bytes(), owner.key().as_ref()],
+      bump,
+      realloc = ANCHOR_SIZE_DESCRIMINATOR + JournalEntryState::INIT_SPACE,
+      realloc::payer = owner,
+      realloc::zero = true
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct DeleteJournalEntry<'info> {
   #[account(mut)]
-  pub journalsolanadapp: Account<'info, Journalsolanadapp>,
+    pub owner: Signer<'info>,
+
+    #[account(
+      mut,
+      seeds = [title.as_bytes(), owner.key().as_ref()],
+      bump,
+      close = owner
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct Journalsolanadapp {
-  count: u8,
+pub struct JournalEntryState {
+    pub owner: Pubkey,
+    #[max_len(50)]
+    pub title: String,
+    #[max_len(1000)]
+    pub content: String,
 }
